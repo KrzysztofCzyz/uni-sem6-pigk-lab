474bcfbede70518a3f7a8a4804e6862afff2f8ec
Krzysztof Czyz <czyz.krzysztof98@gmail.com>
2020-04-08 16:53:58 +0200

    lab 5 - fun

--- main.js
+++ main.js
@@ -2,12 +2,33 @@ function dosrodka(){
     if ($(this).hasClass('animacja-in')){
         $(this).removeClass('animacja-in');
         $(this).addClass('animacja-out');
+        setTimeout(function (object) {
+            $(object).addClass('ani-hover');
+            $(object).removeClass('animacja-out');
+        },1000,this);
     }
     else {
-        $(this).removeClass('animacja-out');
         $(this).addClass('animacja-in');
+        $(this).removeClass('ani-hover');
     }
-
 }
 
+// malo elastyczne jest to setTimeout w tym wypadku
+function dubel(){
+    if ($(this).hasClass('card-double')){
+        $(this).removeClass('card-double');
+        $(this).addClass('animacja-out');
+        setTimeout(function (object) {
+            $(object).addClass('ani-hover');
+            $(object).removeClass('animacja-out');
+        },1000,this);
+    }
+    else {
+        $(this).addClass('card-double');
+        $(this).removeClass('ani-hover');
+    }
+}
+
+$('.card-pic,.card-dd').addClass('ani-hover');
 $('.card-pic').click(dosrodka);
+$('.card-dd').click(dubel);
